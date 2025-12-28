2eb517494b803bb0478b3f6faf41cd26a74ce3e7
Celestia-coder <141723734+Celestia-coder@users.noreply.github.com>
2025-12-28 16:10:06 +0800

    Add fields for on the Fan Interaction Form

---
+++ Models/Form.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Activity3.Models
+{
+    public class Form
+    {
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Character { get; set; }
+        public string? MessageText { get; set; }
+        public int? Rating { get; set; }
+
+
+
+    }
+}
