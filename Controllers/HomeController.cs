40ffd0a37c8abc1a02dcb1851952b10865bedba5
Celestia-coder <141723734+Celestia-coder@users.noreply.github.com>
2025-12-30 17:04:42 +0800

    Add routing to pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,22 @@
-using System.Diagnostics;
 using Activity3.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Activity3.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly FormDbContext _context;
+
+        // Inject DbContext into HomeController
+        public HomeController(ILogger<HomeController> logger, FormDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        // Display Home page (with form)
         public IActionResult Index()
         {
             return View();
@@ -14,6 +25,27 @@ namespace Activity3.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult FanInteractionForm(Form model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Return user back to the form if validation fails
+                return View("Index", model);
+            }
+
+            _context.Forms.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Forms");
+        }
+
+        // Display all submitted forms
+        public IActionResult Forms()
+        {
+            var allForms = _context.Forms.ToList();
+            return View(allForms);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
